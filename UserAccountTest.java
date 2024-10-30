153570ce098d3732effce9f96dc36311c99bd228
Monareng-11 <monarengwinston@gmail.com>
2024-10-30 16:46:30 +0000

    Add files via upload

--- UserAccountTest.java
+++ UserAccountTest.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
+package com.mycompany.main;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -18,21 +19,49 @@ public class UserAccountTest {
     
     public UserAccountTest() {
     }
+    
+    
+    /**
+     * Test of checkUserName method, of class UserAccount.
+     */
+    @Test
+    public void testCheckUserName() {
+        System.out.println("checkUserName");
+        String username = "";
+        UserAccount instance = new UserAccount();
+        boolean expResult = false;
+        boolean result = instance.checkUserName(username);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 
+    /**
+     * Test of checkPasswordComplexity method, of class UserAccount.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+        System.out.println("checkPasswordComplexity");
+        String password = "";
+        UserAccount instance = new UserAccount();
+        boolean expResult = false;
+        boolean result = instance.checkPasswordComplexity(password);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 
     /**
      * Test of registerUser method, of class UserAccount.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testRegisterUser() {
         System.out.println("registerUser");
         String username = "";
         String password = "";
-        String firstName = "";
-        String lastName = "";
         UserAccount instance = new UserAccount();
         String expResult = "";
-        String result = instance.registerUser(username, password, firstName, lastName);
+        String result = instance.registerUser(username, password);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
@@ -41,28 +70,32 @@ public class UserAccountTest {
     /**
      * Test of loginUser method, of class UserAccount.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testLoginUser() {
         System.out.println("loginUser");
         String username = "";
         String password = "";
+        String john = "";
+        String doe = "";
         UserAccount instance = new UserAccount();
         boolean expResult = false;
-        boolean result = instance.loginUser(username, password);
+        boolean result = instance.loginUser(username, password, john, doe);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
     /**
-     * Test of getFullName method, of class UserAccount.
+     * Test of returnloginStatus method, of class UserAccount.
      */
-    @org.junit.jupiter.api.Test
-    public void testGetFullName() {
-        System.out.println("getFullName");
+    @Test
+    public void testReturnloginStatus() {
+        System.out.println("returnloginStatus");
+        String username = "";
+        String password = "";
         UserAccount instance = new UserAccount();
         String expResult = "";
-        String result = instance.getFullName();
+        String result = instance.returnloginStatus(username, password);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
